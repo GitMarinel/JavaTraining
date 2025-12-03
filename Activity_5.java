d681b0855e2aad45ecf13b4079dce06a3ecba9a0
GitMarinel <itcards_issuing_developer1@robinsonsbank.com.ph>
2025-12-03 17:17:47 +0800

    Push Activity 5

---
+++ Activity_5.java
@@ -0,0 +1,14 @@
+package ph.com.bpi.hello;
+
+public class Activity_5 {
+
+	public static void main(String[] args) {
+		// Sum by adding all numbers
+		int sum = 0;
+		for(int num = 0; num <= 50; num++) {
+			sum += num;
+		}
+		System.out.print("Sum: " + sum);
+	}
+
+}
