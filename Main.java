ce35e359e2c93e3b170baa30bce005c437d0c431
Marinel De Leon <gstmdeleon@robinsonsbank.com.ph>
2025-12-04 17:47:37 +0800

    Update Exercise1

--- Main.java
+++ Main.java
@@ -8,20 +8,23 @@ public class Main {
 		// TODO Auto-generated method stub
 				Scanner scn = new Scanner(System.in);
 			
+		//Get User input
+		System.out.print("Enter brand for car1: ");
+		String brand = scn.nextLine();
+		System.out.print("Enter color for car1: ");
+		String color = scn.nextLine();
+		System.out.println("");
+		
+		//Setting details for Car1
+		Car car1 = new Car(brand, color, 2);
+		//Show the Details for Car1
+		car1.Show();
+		Car car2 = new Car();
+		System.out.println("");
+		//Show the Details for Car2
+		car2.Show();
 				
-				System.out.print("Enter brand for car1: ");
-				String brand = scn.nextLine();
-				System.out.print("Enter color for car1: ");
-				String color = scn.nextLine();
-				System.out.println("");
-				
-				Car car1 = new Car(brand, color, 2);
-				car1.Show();
-				Car car2 = new Car();
-				System.out.println("");
-				car2.Show();
-				
-				scn.close();
+		scn.close();
 	}
 
 }
