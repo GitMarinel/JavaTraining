800f5cad688380a3cefe7047d5d1a72b201e1c6f
Marinel De Leon <gstmdeleon@robinsonsbank.com.ph>
2025-12-04 16:35:03 +0800

    Push Seat Work 2

---
+++ SeatWork2.java
@@ -0,0 +1,14 @@
+package ph.com.bpi.oop.SeatWork2;
+
+public class SeatWork2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WhiteBoard wb = new WhiteBoard();
+		int total = wb.addNumbers(5, 3);
+		
+		System.out.println("The total is: " + String.valueOf(total));
+	}
+
+}
---
+++ WhiteBoard.java
@@ -0,0 +1,10 @@
+package ph.com.bpi.oop.SeatWork2;
+
+public class WhiteBoard {
+	
+	
+	int addNumbers(int addend1, int addend2) {
+		int ans = addend1 + addend2;
+		return ans;
+	}
+}
