641048ca7f3ff979b3753381381a33a3d82985d5
GitMarinel <itcards_issuing_developer1@robinsonsbank.com.ph>
2026-01-16 16:34:37 +0800

    Push M4_Activity8

---
+++ InvalidAmountException.java
@@ -0,0 +1,7 @@
+package ph.com.bpi.oop.m4_activity8;
+
+public class InvalidAmountException extends Exception {
+    public InvalidAmountException(String message) {
+        super(message);
+    }
+}
